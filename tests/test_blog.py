a2b09d547981eecf9f007594ff1e25dc6e41bc5f
Alex Pask <pask200@gmail.com>
2020-03-15 16:34:39 +0000

    Break UTs

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -2,7 +2,9 @@ import pytest
 
 from flaskr.db import get_db
 
-
+def test_fail():
+    assert 1 == 2
+    
 def test_index(client, auth):
     response = client.get("/")
     assert b"Log In" in response.data
